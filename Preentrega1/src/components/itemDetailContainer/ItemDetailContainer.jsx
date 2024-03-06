fb20f09a379c3522dbab5c649e5b0062509cf7fb
Emiliano Cabrera Geido <emilianocabrerageido@gmail.com>
2024-03-06 12:41:06 -0300

    added itemDetail

--- Preentrega1/src/components/itemDetailContainer/ItemDetailContainer.jsx
+++ Preentrega1/src/components/itemDetailContainer/ItemDetailContainer.jsx
@@ -1,58 +1,60 @@
-import React, { useEffect, useState } from "react";
+import React from 'react'
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Card, CardBody, CardFooter, Image, Stack, Heading, Button, Text } from '@chakra-ui/react';
+import { Card,CardBody, CardFooter, Image, Stack, Heading, Button, Text } from '@chakra-ui/react'
 import { getProductsById } from "../../data/asyncMock";
 
-const ItemDetailContainer = () => {
-    const [product, setProduct] = useState({});
-    const [loading, setLoading] = useState(true);
-    const { productID } = useParams();
-  
-    useEffect(() => {
-        setLoading(true);
-      
-        getProductsById(productID)
-          .then((el) => {
-            console.log(productID);
-            setProduct(el);
-            setLoading(false);
-          })
-          .catch((err) => console.log(err));
-      
-      }, [productID]);
-  
-    return (
-      <Card
-        direction={{ base: 'column', sm: 'row' }}
-        overflow='hidden'
-        variant='outline'
-      >
-        {product && product.img && ( // Verifica si product y product.img están definidos antes de acceder a sus propiedades
-          <Image
-            objectFit='cover'
-            maxW={{ base: '100%', sm: '200px' }}
-            src={product.img}
-            alt={product.nombre}
-          />
-        )}
-  
-        <Stack>
-          <CardBody>
-            <Heading size='md'>{product && product.nombre}</Heading> {/* Verifica si product está definido antes de acceder a su propiedad 'nombre' */}
-            <Text py='2'>
-              Caffè latte is a coffee beverage of Italian origin made with espresso
-              and steamed milk.
-            </Text>
-          </CardBody>
-  
-          <CardFooter>
-            <Button variant='solid' colorScheme='blue'>
-              Buy Latte
-            </Button>
-          </CardFooter>
-        </Stack>
-      </Card>
-    );
-};
 
-export default ItemDetailContainer;
+const ItemDetailContainer = () => {
+    
+        const [product, setProducts] = useState({});
+        const [loading, setLoading] = useState(true);
+        const {productID} = useParams()
+      
+        useEffect(() => {
+          setLoading(true)
+         
+          getProductsById(productID)
+          .then ((el)=> setProducts(el))
+          .catch((err)=> console.log(err))
+          .finally(()=> setLoading(false))
+         
+        }, []);
+        console.log(productID)
+        console.log(product)
+
+    
+  return (
+    <Card
+  direction={{ base: 'column', sm: 'row' }}
+  overflow='hidden'
+  variant='outline'
+>
+  <Image
+    objectFit='cover'
+    maxW={{ base: '100%', sm: '200px' }}
+    src={product.img}
+    alt='Caffe Latte'
+  />
+
+  <Stack>
+    <CardBody>
+      <Heading size='md'>{product.nombre}</Heading>
+
+      <Text py='2'>
+        Caffè latte is a coffee beverage of Italian origin made with espresso
+        and steamed milk.
+      </Text>
+    </CardBody>
+
+    <CardFooter>
+      <Button variant='solid' colorScheme='blue'>
+        Buy Latte
+      </Button>
+    </CardFooter>
+  </Stack>
+</Card>
+  )
+}
+
+export default ItemDetailContainer
